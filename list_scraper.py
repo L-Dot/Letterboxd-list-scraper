0e2067ddd0de1b481a2b70b7326b5b5b95d3f5c2
Den Jackson <denjacksonsemail@gmail.com>
2023-11-30 23:42:52 -0600

    Finalized scraping additions

--- list_scraper.py
+++ list_scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from tqdm import tqdm
 import numpy as np
+import re
 
 _domain = 'https://letterboxd.com/'
 
@@ -69,7 +70,38 @@ def scrape_list(list_link):
             except:
                 average_rating = np.nan
 
-            film_rows.append([film_name, release_year, director, cast, rating, average_rating, _domain+film_card])
+            # Scrape movie genres
+            genres = film_soup.find('div', {'class': 'text-sluglist capitalize'})
+            genres = [genres.text for genres in genres.find_all('a', {'class': 'text-slug'})]
+
+            # Get movie runtime by searching for first sequence of digits in the p element with the runtime
+            runtime = int(re.search(r'\d+', film_soup.find('p', {'class': 'text-link text-footer'}).text).group())
+
+            # In order to get stats on how many people watched/liked the movie, need to use new link
+
+            # This grabs the name of the movie as it appears in the url
+            movie = film_page.split('/')[-2]
+            # This goes to the page with the desired stats for the movie
+            r = requests.get(f'https://letterboxd.com/esi/film/{movie}//stats/')
+            stats_soup = BeautifulSoup(r.content, 'lxml')
+
+            # Get number of people that have watched the movie
+            # Grab the full "Watched by X members" string
+            watches = stats_soup.find('a', {'class': 'has-icon icon-watched icon-16 tooltip'})["title"]
+            # Filters above message to only grab the number portion
+            watches = re.findall(r'\d+', watches)
+            # Numbers are separated by the comma, join them back together to get actual count
+            watches = int(''.join(watches))
+
+            # Get number of people that have liked the movie
+            # Grab the full "Liked by X members" string
+            likes = stats_soup.find('a', {'class': 'has-icon icon-like icon-liked icon-16 tooltip'})["title"]
+            # Filters above message to only grab the number portion
+            likes = re.findall(r'\d+', likes)
+            # Numbers are separated by the comma, join them back together to get actual count
+            likes = int(''.join(likes))
+
+            film_rows.append([film_name, release_year, director, genres, rating, average_rating, runtime, watches, likes, cast, film_page])
             
         # check if there is another page of ratings
         next_button = soup.find('a', class_='next')
