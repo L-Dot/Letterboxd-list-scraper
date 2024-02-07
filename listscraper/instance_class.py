d6e7f76d10fb8a1fb8f0982b608f19a35d594af2
Arno Lafontaine <43766835+L-Dot@users.noreply.github.com>
2024-02-07 18:08:57 +0100

    Code cleanup

--- listscraper/instance_class.py
+++ listscraper/instance_class.py
@@ -17,7 +17,7 @@ class ScrapeInstance:
         output_path (str):      Output path obtained from optional '-op' flag. Default is 'scraper_outputs' directory.
         infile (str):           Name of input .txt file obtained from optional '-f' flag.
         concat (bool):          Option to turn on list concatenation read from optional '--concat' flag. Default is False.
-        quiet(bool):      Turn off tqdm loading bars read from optional '-vo' flag. Default is False.
+        quiet(bool):            Turn off tqdm loading bars read from optional '-vo' flag. Default is False.
         threads (int):          Amount of threads used for scraping read from optional '--threads' flag. Default is 4. 
 
     Methods:
@@ -214,84 +214,3 @@ class ScrapeInstance:
                 write.writerows(self.concat_lists)
             
             return print(f"    Written concatenated lists to {self.global_output_name}.csv!")
-        
-
-# if __name__ == "__main__":
-
-#     parser=argparse.ArgumentParser(prog="listscraper", usage="%(prog)s [options] [list-url]",
-#                                    description="A Python program that scrapes Letterboxd lists and outputs the information on all of its films in a CSV file. Input form and desired outputs are user customizable. Concurrent scraping of multiple lists is supported.",
-#                                    epilog="Thank you for using the scraper! If you have any trouble and/or suggestions please contact me via the GitHub page https://github.com/L-Dot/Letterboxd-list-scraper.",
-#                                    formatter_class=argparse.RawTextHelpFormatter)
-#     parser.add_argument('--version', action='version', version='%(prog)s 2.0.0')
-
-#     ## arguments / flags
-#     parser.add_argument("-on", "--output_name", type=str,
-#                         help=("set the filename of the output CSV(s). Default output is a CSV file with the same name as its respective list.\n"
-#                               "If multiple URLs are provided, each CSV will be the concatenation of the filename with an increasing number _1, _2, etc.\n"
-#                               "The original list name will be output in the CSV."), 
-#                         required=False, default=None)
-    
-#     parser.add_argument("-op", "--output_path", type=str, 
-#                         help="set the path for the output CSV(s). Default output is a folder called 'scraper_outputs'.", 
-#                         required=False, default="scraper_outputs")
-    
-#     parser.add_argument("-f", "--file", type=argparse.FileType('r'),
-#                         help="provide a .txt file with all of the URLs that should be scraped. Each URL should be isolated on its own newline together with any specific option flags. \
-#                             This feature is especially handy when you need to scrape a large amount of lists and/or when you have specific option flags for each URL.\n\
-#                             When using an input file, make sure to not give the script any URLs on the command line!", 
-#                         required=False, default=None)
-
-#     parser.add_argument("-p","--pages", type=str,
-#                         help=("only scrape selected pages from a URL. Default is to scrape all pages.\n"
-#                               "Page selection syntax follows these rules:\n"
-#                               "\t -p 1 \t\t page 1 (first page)\n"
-#                               "\t -p 1,3,5 \t pages 1,3,5\n"
-#                               "\t -p 1~3 \t pages 1,2,3\n"
-#                               "\t -p 1~3,5 \t pages 1,2,3 and 5\n"
-#                               "\t -p '<3,5' \t pages 1,2,3 and 5\n"
-#                               "\t -p '*' \t\t all pages (default)\n"
-#                               "The string should contain NO spaces. Also note the requirement of quotation marks when using the less-than (<) or star (*) signs!"),
-#                         required=False, default="*")
-
-#     # parser.add_argument("--more-stats", action=argparse.BooleanOptionalAction,
-#     #                     help="option to enable extensive scraping, at the cost of a small increase in runtime. New data includes info on a film's rating histogram and fans.",
-#     #                     required=False, default=False)
-
-#     # parser.add_argument("--meta-data", action=argparse.BooleanOptionalAction,
-#     #                     help="option to add a header row with some meta data to the CSV.",
-#     #                     required=False, default=False)
-
-#     parser.add_argument("--concat", action=argparse.BooleanOptionalAction,
-#                         help="option to output all the scraped lists into a single concatenated CSV. An extra column is added that specifies the original list URL.",
-#                         required=False, default=False)
-    
-#     parser.add_argument("--threads", type=int,
-#                         help="option to tweak the number of CPU threads used. Increase this to speed up scraping of multiple lists simultaneously. Default value is 4.",
-#                         required=False, default=4)
-
-#     parser.add_argument("--verbose-off", action=argparse.BooleanOptionalAction,
-#                         help="Turn off verbose, stops describing everything the program does and no longer displays tqdm() progression bars.\
-#                         From testing this does not significantly increase program runtime, meaning verbose is turned on by default.",
-#                         required=False,default=False)
-
-#     ## positionals
-#     parser.add_argument("listURL", type=str, nargs="*", 
-#                         help=("the Letterboxd URL of the list that should be scraped. Multiple URLs can be given, separated by a space. For example:\n\n"
-#                         "\t python %(prog)s.py <list-url_1>\n"
-#                         "\t python %(prog)s.py <list-url_1> <list-url_2> <list-url_3>"))
-
-#     args=parser.parse_args()
-
-
-#     ## Starts the program
-
-#     print("=============================================")
-#     print("           Letterboxd-List-Scraper           ")
-#     print("=============================================")
-
-#     LBscraper = LBlistscraper(args.listURL, args.pages, args.output_name, args.output_path, args.file, args.concat, args.quiet, args.threads)
-
-#     # Ends the program
-
-#     print(f"\nProgram successfully finished! Your CSV(s) can be found in the ./{LBscraper.output_path}/.")
-#     print(f"    Total run time was {LBscraper.endtime - LBscraper.starttime :.2f} seconds.")
